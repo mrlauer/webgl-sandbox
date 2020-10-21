31feb512611657f32747a5f9d24ffb044593f99e
Michael Lauer <mrlauer@gmail.com>
2020-10-21 18:18:40 +0000

    more of same

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,5 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Image::Application.config.secret_token = '92433729b56efee5b07d9c079afd7ce78d7972f36738a9cf85107cf0dddadb7361f7eb0ac19ae8bd7cf555ffa4121e15710bbbae51118b299e044491ba415fe2'
-Babybrain::Application.config.secret_key_base = '336add7a50f5d8fa7d8548f3d619e84e07dddd94afcae6b0dee082bec334caeaca301b4e92a9d704787d3bf5ad5f175cd797c95a6ca0bb168e5d39f5c714f379'
+Image::Application.config.secret_key_base = '336add7a50f5d8fa7d8548f3d619e84e07dddd94afcae6b0dee082bec334caeaca301b4e92a9d704787d3bf5ad5f175cd797c95a6ca0bb168e5d39f5c714f379'
 
